5c568c3782008f71190f8a2e0963f5272ef50e5d
Álvaro Rivas <alvarora12@gmail.com>
2021-03-24 14:01:37 +0100

    proyecto inicializado

---
+++ fitosanitarios/src/main/java/es/codeurjc/fitosanitarios/FitosanitariosApplication.java
@@ -0,0 +1,13 @@
+package es.codeurjc.fitosanitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitosanitariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitosanitariosApplication.class, args);
+	}
+
+}
