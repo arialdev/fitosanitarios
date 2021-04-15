44f95185c969fe855b493fd9271460533d0542ae
Álvaro Rivas <alvarora12@gmail.com>
2021-04-15 21:36:47 +0200

    Añadir tratamiento en la vista de nuevo cultivo + animación de añadir

--- fitosanitarios/src/main/java/es/codeurjc/fitosanitarios/controllers/HomeController.java
+++ fitosanitarios/src/main/java/es/codeurjc/fitosanitarios/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public String homeRedirection() {
+    public String loadHome() {
         return "home-view";
     }
 }
